9bab5e75018b9dcf82229fa87f1e38b8636390dc
Frees3c <freesec@xps.com>
2021-05-30 12:21:32 +0100

    lsp.diagnostics update_on_insert for python

--- lua/lsp/python-ls.lua
+++ lua/lsp/python-ls.lua
@@ -3,14 +3,14 @@ require'lspconfig'.pyright.setup {
     cmd = {DATA_PATH .. "/lspinstall/python/node_modules/.bin/pyright-langserver", "--stdio"},
     on_attach = require'lsp'.on_attach,
 	filetypes = {"python"},
-    -- handlers = {
-    --     ["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
-    --         virtual_text = true,
-    --         signs = true,
-    --         underline = true,
-    --         update_in_insert = true
-    --     })
-    -- },
+    handlers = {
+        ["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
+            virtual_text = true,
+            signs = true,
+            underline = true,
+            update_in_insert = true
+        })
+    },
 	 settings = {
       python = {
         analysis = {
