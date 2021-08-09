e8ac666606c2abb89d616ec2b588eef9ea882e89
Frees3c <freesec@xps.com>
2021-08-09 21:26:26 +0000

    added last-postion jump to autocmds

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -1,7 +1,7 @@
 vim.cmd [[augroup Autogroup]]
 
+vim.cmd [[au TextYankPost * silent! lua vim.highlight.on_yank ]]
 
-vim.cmd [[au TextYankPost * silent! lua vim.highlight.on_yank {higroup="HighlightedyankRegion", timeout=150}]]
-
+vim.cmd [[ autocmd BufReadPost * if line("'\"") >= 1 && line("'\"") <= line("$") && &ft !~# 'commit' |   exe "normal! g`\"" | endif ]]
 
 vim.cmd [[augroup END]]
