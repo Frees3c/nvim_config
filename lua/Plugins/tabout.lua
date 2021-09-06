c47979ac66836de5565347d3487c9e187a4c6168
Frees3c <freesec@xps.com>
2021-09-06 17:49:25 +0000

    modified tabout.lua

--- lua/Plugins/tabout.lua
+++ lua/Plugins/tabout.lua
@@ -1,18 +1,22 @@
-require('tabout').setup {}
--- require('tabout').setup({
---     tabkey = "<Tab>", -- key to trigger tabout
---     act_as_tab = true, -- shift content if tab out is not possible
---     completion = true, -- if the tabkey is used in a completion pum
---     tabouts = {
---         {open = "'", close = "'"},
---         {open = '"', close = '"'},
---         {open = '`', close = '`'},
---         {open = '(', close = ')'},
---         {open = '[', close = ']'},
---         {open = '{', close = '}'}
---     },
---     ignore_beginning = true, --[[ if the cursor is at the beginning of a filled element it will rather tab out than shift the content ]]
---     exclude = {} -- tabout will ignore these filetypes
--- })
--- 
--- 
+local M = {}
+
+function M.config()
+    require('tabout').setup {
+        tabkey = '',
+        backwards_tabkey = '',
+        -- act_as_tab = false,
+        -- completion = true,
+        tabouts = {
+            { open = '\'', close = '\'' },
+            { open = '"', close = '"' },
+            { open = '`', close = '`' },
+            { open = '(', close = ')' },
+            { open = '[', close = ']' },
+            { open = '{', close = '}' },
+        },
+        -- ignore_beginning = true,
+        -- exclude = {},
+    }
+end
+
+return M
