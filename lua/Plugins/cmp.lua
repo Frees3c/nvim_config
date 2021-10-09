5113f26568a8d9b6d95902226c36b53c39e54257
Frees3c <freesec@xps.com>
2021-10-09 21:33:43 +0000

    Fixed tab compltion in nvim-cmp using (https://github.com/hrsh7th/nvim-cmp/issues/286)

--- lua/Plugins/cmp.lua
+++ lua/Plugins/cmp.lua
@@ -45,29 +45,23 @@ cmp.setup {
 
   -- ... Your other configuration ...
 
-  mapping = {
-    ["<TAB>"] = cmp.mapping(function(fallback)
-      if vim.fn.pumvisible() == 1 then
-        feedkey("<C-n>", "n")
-      elseif vim.fn["vsnip#available"]() == 1 then
-        feedkey("<Plug>(vsnip-expand-or-jump)", "")
-      elseif has_words_before() then
-        cmp.complete()
-      else
-        fallback() -- The fallback function sends a already mapped key. In this case, it's probably `<Tab>`.
-      end
-    end, { "i", "s" }),
-
-    ["<S-TAB>"] = cmp.mapping(function()
-      if vim.fn.pumvisible() == 1 then
-        feedkey("<C-p>", "n")
-      elseif vim.fn["vsnip#jumpable"](-1) == 1 then
-        feedkey("<Plug>(vsnip-jump-prev)", "")
-      end
-    end, { "i", "s" }),
-    ['<CR>'] = cmp.mapping.confirm(),
-
-  },
+    mapping = {
+      -- other mappings
+      ["<Tab>"] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          cmp.select_next_item()
+        else
+          fallback()
+          end
+        end, { "i" }),
+      ["<S-Tab>"] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          cmp.select_prev_item()
+        else
+          fallback()
+          end
+        end, { "i" }),
+      },
   formatting = {
       format = function(entry, vim_item)
         local icons = require("lsp.kind").icons
