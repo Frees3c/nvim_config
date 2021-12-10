cd54cc23c988a948d3311eead67e40fad9bd71f9
Frees3c <freesec@xps.com>
2021-12-10 21:03:47 +0000

    more lspsaga stuff

--- lua/Plugins/lspsaga.lua
+++ lua/Plugins/lspsaga.lua
@@ -1,16 +1,8 @@
 require("lspsaga").init_lsp_saga {
-				error_sign = '❌',
-				warn_sign = '⚠️',
-				hint_sign = '💡',
-				infor_sign = 'ℹ️',
-				dianostic_header_icon = ' 🚒 ',
-				code_action_icon = '💡',
 				code_action_keys = {
 					quit = '<esc>',
 					exec = '<cr>'
 				},
-				finder_definition_icon = '📖 ',
-				finder_reference_icon = '🔖 ',
 				finder_action_keys = {
 					open = '<cr>',
 					split = 's',
@@ -18,10 +10,6 @@ require("lspsaga").init_lsp_saga {
 					quit = '<esc>',
 					scroll_down = '<c-f>',
 					scroll_up = '<c-b>'
-				},
-				code_action_keys = {
-					quit = '<esc>',
-					exec = '<cr>'
 				},
 				rename_action_keys = {
 					quit = '<esc>',
