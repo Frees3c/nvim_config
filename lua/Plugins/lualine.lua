fa3142f9051404c706ca8fa99273eb6a0b082701
Frees3c <freesec@xps.com>
2021-12-10 20:59:38 +0000

    messing with lspsaga

--- lua/Plugins/lualine.lua
+++ lua/Plugins/lualine.lua
@@ -13,7 +13,7 @@ require('lualine').setup {
     lualine_b = {'branch', 'diff'},
     lualine_c = {'filename'},
     lualine_x = {
-            {"diagnostics", sources = {"nvim_lsp"}},
+      {'diagnostics', sources = {'nvim_diagnostic'}},
             'filetype',
             'encoding'
     },
