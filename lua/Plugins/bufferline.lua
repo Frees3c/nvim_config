fb457b71b2a0b7450dd3090397c689358d689146
Frees3c <joeyjordanbarnes@hotmail.com>
2021-12-26 23:45:16 +0000

    added config for `bufferline`

--- lua/Plugins/bufferline.lua
+++ lua/Plugins/bufferline.lua
@@ -1,14 +1,44 @@
-require "bufferline".setup {}
+require "bufferline".setup {
+    options = {
+        numbers = function(opts)
+            return string.format("%s", opts.raise(opts.ordinal))
+        end,
+        separator_style = "slant",
+        offsets = {
+            {
+                filetype = "NvimTree",
+                text = function()
+                    return vim.fn.getcwd()
+                end,
+                highlight = "Directory",
+                text_align = "left"
+            }
+        },
+        custom_areas = {
+            right = function()
+                local result = {}
+                local error = vim.lsp.diagnostic.get_count(0, [[Error]])
+                local warning = vim.lsp.diagnostic.get_count(0, [[Warning]])
+                local info = vim.lsp.diagnostic.get_count(0, [[Information]])
+                local hint = vim.lsp.diagnostic.get_count(0, [[Hint]])
 
--- These commands will navigate through buffers in order regardless of which mode you are using
--- key_mapper('n', '<TAB>', ':BufferLineCycleNext<CR>')
--- key_mapper('n', '<S-TAB>', ':BufferLineCyclePrev<CR>')
+                if error ~= 0 then
+                    table.insert(result, {text = "  " .. error, guifg = "#EC5241"})
+                end
 
--- These commands will move the current buffer backwards or forwards in the bufferline
--- nnoremap <silent><mymap> :BufferLineMoveNext<CR>
--- nnoremap <silent><mymap> :BufferLineMovePrev<CR>
--- --
--- -- These commands will sort buffers by directory, language, or a custom criteria
--- nnoremap <silent>be :BufferLineSortByExtension<CR>
--- nnoremap <silent>bd :BufferLineSortByDirectory<CR>
--- nnoremap <silent><mymap> :lua require'bufferline'.sort_buffers_by(function (buf_a, buf_b) return buf_a.id < buf_b.id end)<CR>
+                if warning ~= 0 then
+                    table.insert(result, {text = "  " .. warning, guifg = "#EFB839"})
+                end
+
+                if hint ~= 0 then
+                    table.insert(result, {text = "  " .. hint, guifg = "#A9BA1E"})
+                end
+
+                if info ~= 0 then
+                    table.insert(result, {text = "  " .. info, guifg = "#2EA9A7"})
+                end
+                return result
+            end
+        }
+    }
+}
