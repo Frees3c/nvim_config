755cf012c48106b7ea53c6a3f81e3d5350d8b981
Frees3c <joeyjordanbarnes@hotmail.com>
2022-02-23 20:47:29 +0000

    added `packer` settings from Christianchiarull's `LunarVim`, (auto install and popup window)

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -8,17 +8,47 @@
 --
 -- ###################################################################################
 
-local execute = vim.api.nvim_command
 local fn = vim.fn
 
+-- Install packer
 local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
-
 if fn.empty(fn.glob(install_path)) > 0 then
-    execute("!git clone https://github.com/wbthomason/packer.nvim " .. install_path)
-    execute "packadd packer.nvim"
+    PACKER_BOOTSTRAP =
+        fn.system {
+        "git",
+        "clone",
+        "--depth",
+        "1",
+        "https://github.com/wbthomason/packer.nvim",
+        install_path
+    }
+    print "Installing packer close and reopen Neovim..."
+    vim.cmd [[packadd packer.nvim]]
 end
 
-return require("packer").startup(
+-- Autocommand that reloads neovim whenever you save the plugins.lua file
+vim.cmd [[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerSync
+  augroup end
+]]
+
+-- Use a protected call so we don't error out on first use
+local status_ok, packer = pcall(require, "packer")
+if not status_ok then
+    return
+end
+
+-- Have packer use a popup window
+packer.init {
+    display = {
+        open_fn = function()
+            return require("packer.util").float {border = "rounded"}
+        end
+    }
+}
+return packer.startup(
     function(use)
         -- Packer can manage itself as an optional plugin
         use "wbthomason/packer.nvim"
@@ -39,18 +69,19 @@ return require("packer").startup(
         -- Plugin dev
         use "folke/lua-dev.nvim"
 
-        -- Auto completion
+        -- cmp plugins(autocompltetion)
         use "hrsh7th/nvim-cmp"
         use "hrsh7th/cmp-nvim-lsp"
         use "hrsh7th/cmp-buffer"
         use "hrsh7th/cmp-vsnip"
         use "hrsh7th/cmp-path"
         use "hrsh7th/cmp-emoji"
+        use "hrsh7th/cmp-cmdline"
         use "hrsh7th/cmp-latex-symbols"
-        use "hrsh7th/vim-vsnip"
         use "hrsh7th/cmp-nvim-lua"
 
         -- Snippets
+        use "hrsh7th/vim-vsnip"
         use "rafamadriz/friendly-snippets"
 
         -- Debugging
@@ -114,5 +145,8 @@ return require("packer").startup(
         use "kdav5758/TrueZen.nvim"
         use "folke/which-key.nvim"
         use {"iamcco/markdown-preview.nvim", run = "cd app && npm install"}
+        if PACKER_BOOTSTRAP then
+            require("packer").sync()
+        end
     end
 )
