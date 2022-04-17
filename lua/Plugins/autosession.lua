a763bcca298be118393534af2654e648fc84ed1c
Frees3c <joeyjordanbarnes@hotmail.com>
2022-04-18 00:23:33 +0100

    disables auto-create

--- lua/Plugins/autosession.lua
+++ lua/Plugins/autosession.lua
@@ -1,3 +1,7 @@
 require("auto-session").setup {
-    log_level = "error",
+  log_level = "error",
+  auto_session_create_enabled = "false",
+  auto_session_use_git_branch = "true",
+  auto_restore_enabled = "false"
 }
+vim.o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal"
